fb6cf2b3f6cb3bd9c28f209efc2654a844263ffe
Joerg Henrichs <henrichsjoerg@gmail.com>
2026-08-25 13:26:02 +1000

    #5 Updated Fab script to support latest Fab changes.A

--- fab/site_specific/default/config.py
+++ fab/site_specific/default/config.py
@@ -15,11 +15,13 @@ from typing import List
 
 from fab.api import AddFlags, BuildConfig, Category, ToolRepository
 
-from default.setup_script_cray import setup_script_cray
-from default.setup_script_gnu import setup_script_gnu
-from default.setup_script_intel_classic import setup_script_intel_classic
-from default.setup_script_intel_llvm import setup_script_intel_llvm
-from default.setup_script_nvidia import setup_script_nvidia
+from site_specific.default.setup_script_cray import setup_script_cray
+from site_specific.default.setup_script_gnu import setup_script_gnu
+from site_specific.default.setup_script_intel_classic import (
+    setup_script_intel_classic)
+from site_specific.default.setup_script_intel_llvm import (
+    setup_script_intel_llvm)
+from site_specific.default.setup_script_nvidia import setup_script_nvidia
 
 
 class Config:
@@ -92,7 +94,9 @@ class Config:
         # the linker):
         tr = ToolRepository()
         for compiler in (tr[Category.C_COMPILER] +
+                         tr[Category.C_PREPROCESSOR] +
                          tr[Category.FORTRAN_COMPILER] +
+                         tr[Category.FORTRAN_PREPROCESSOR] +
                          tr[Category.LINKER]):
             # Define a base profile, which contains the common
             # compilation flags. This 'base' is not accessible to
